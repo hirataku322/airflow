30ca0f454c748df3bdf4309e8e0977b8e0f1530f
hyamakaw <hirotoyamakawa@yamakawatakutos-MacBook-Air.local>
2024-02-17 16:48:15 +0900

    fix: pip versions

--- install_airflow.sh
+++ install_airflow.sh
@@ -7,4 +7,4 @@ PYTHON_VERSION="$(python3 --version | cut -d " " -f 2 | cut -d "." -f 1-2)"
 CONSTRAINT_URL="https://raw.githubusercontent.com/apache/airflow/constraints-${AIRFLOW_VERSION}/constraints-${PYTHON_VERSION}.txt"
 # For example this would install 2.8.1 with python 3.8: https://raw.githubusercontent.com/apache/airflow/constraints-2.8.1/constraints-3.8.txt
 
-pip install "apache-airflow==${AIRFLOW_VERSION}" --constraint "${CONSTRAINT_URL}"
+pip3 install "apache-airflow==${AIRFLOW_VERSION}" --constraint "${CONSTRAINT_URL}"
